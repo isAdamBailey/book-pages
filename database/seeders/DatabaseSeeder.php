65885b7fd5273220844b23c2839c318579274b73
Adam Bailey <adamjbailey7@gmail.com>
2022-05-10 21:42:14 -0700

    Create books and pages

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             RolesAndPermissionsSeeder::class,
-            SiteSeeder::class
+            SiteSeeder::class,
+            BookSeeder::class
         ]);
     }
 }
