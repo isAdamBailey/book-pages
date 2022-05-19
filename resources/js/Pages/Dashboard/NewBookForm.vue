731c9192632c453eb7d2aeb14492436730831e01
Adam Bailey <adamjbailey7@gmail.com>
2022-05-18 21:19:10 -0700

    add newbook form vue component

---
+++ resources/js/Pages/Dashboard/NewBookForm.vue
@@ -0,0 +1,47 @@
+<script setup>
+import BreezeButton from '@/Components/Button.vue';
+import BreezeInput from '@/Components/Input.vue';
+import BreezeLabel from '@/Components/Label.vue';
+import BreezeValidationErrors from '@/Components/ValidationErrors.vue';
+import {useForm} from '@inertiajs/inertia-vue3';
+
+const form = useForm({
+  title: '',
+  excerpt: '',
+  author: ''
+});
+
+const submit = () => {
+  form.post(route('books.store'), {});
+};
+</script>
+
+<template>
+  <BreezeValidationErrors class="mb-4"/>
+
+  <form @submit.prevent="submit">
+    <div>
+      <BreezeLabel for="title" value="Title"/>
+      <BreezeInput id="title" type="text" class="mt-1 block w-full" v-model="form.title" required autofocus
+                   autocomplete="title"/>
+    </div>
+
+    <div class="mt-4">
+      <BreezeLabel for="excerpt" value="Excerpt"/>
+      <BreezeInput id="excerpt" type="text" class="mt-1 block w-full" v-model="form.excerpt"
+                   autocomplete="excerpt"/>
+    </div>
+
+    <div class="mt-4">
+      <BreezeLabel for="author" value="Author Name"/>
+      <BreezeInput id="author" type="text" class="mt-1 block w-full" v-model="form.author"
+                   autocomplete="author"/>
+    </div>
+
+    <div class="flex items-center justify-end mt-4">
+      <BreezeButton class="ml-4" :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+        Create!
+      </BreezeButton>
+    </div>
+  </form>
+</template>
