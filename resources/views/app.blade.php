2320c08544b87f7c9e279ee1421a1880cece8111
Adam Bailey <adamjbailey7@gmail.com>
2022-06-08 22:55:17 -0700

    try to disable context menus in app

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -17,7 +17,7 @@
         <script src="{{ mix('js/app.js') }}" defer></script>
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased" oncontextmenu="return false;">
         @inertia
 
         @env ('local')
