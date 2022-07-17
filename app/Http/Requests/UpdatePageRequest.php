30578e4cc3c67d334e90a1c99bd3c619ffaeea44
Adam Bailey <adamjbailey7@gmail.com>
2022-07-16 22:27:01 -0700

    allow 40m file uploads

--- app/Http/Requests/UpdatePageRequest.php
+++ app/Http/Requests/UpdatePageRequest.php
@@ -25,7 +25,7 @@ class UpdatePageRequest extends FormRequest
     {
         return [
             'content' => 'string',
-            'image' => 'max:20000,mimes:jpg,jpeg,bmp,png,svg,webp,avi,gif,mpeg,quicktime,mp4',
+            'image' => 'max:40000,mimes:jpg,jpeg,bmp,png,svg,webp,avi,gif,mpeg,quicktime,mp4',
         ];
     }
 }
